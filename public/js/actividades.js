c32897257ec953dd7b1a707c89c40a7710475c65
Victor Echeverri <162375245+vme22@users.noreply.github.com>
2024-07-23 08:46:33 -0500

    fecha exportacion

--- public/js/actividades.js
+++ public/js/actividades.js
@@ -66,7 +66,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         {
           extend: "excelHtml5",
           text: '<i class="fas fa-file-excel"></i> &nbsp;Exportar Excel',
-          title: 'Pruebas de Conocimiento',
           filename: function () {
             var d = new Date();
             var date =
@@ -77,20 +76,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             columns: ":visible",
             format: {
               body: function (data, row, column, node) {
-                var selectElement = $(node).find("select");
-                if (selectElement.length > 0) {
-                  return $(node).find(".selected-resultado").val();
-                }
-                var checkboxElement = $(node).find('input[type="checkbox"]');
-                if (checkboxElement.length > 0) {
-                  return "";
-                }
-                return data.replace(/<\/?[^>]+(>|$)/g, ""); // Remueve etiquetas HTML
-              },
+                return extractTextFromNode(node);
+              }
             },
           },
           customize: function (xlsx) {
-            var sheet = xlsx.xl.worksheets["sheet1.xml"];
             var d = new Date();
             var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
             var time = d.toLocaleString('es-CO', {
@@ -104,29 +94,12 @@ document.addEventListener('DOMContentLoaded', async () => {
             var additionalData = "Calificaciones Centro de Actividades";
 
             // Crear una nueva fila con la fecha y el dato adicional
-            var row = '<row r="1"><c t="inlineStr" r="A1"><is><t>' + formattedDate + ' ' + additionalData + "</t></is></c></row>";
-
-            // Ajustar los índices de las filas existentes
-            $('row', sheet).each(function () {
-              var r = parseInt($(this).attr('r'));
-              $(this).attr('r', r + 1);
-              $('c', this).each(function () {
-                var ref = $(this).attr('r');
-                var col = ref.substring(0, 1);
-                var row = parseInt(ref.substring(1)) + 1;
-                $(this).attr('r', col + row);
-              });
-            });
+            var row =
+              '<row r="1"><c t="inlineStr" r="A1"><is><t>' + formattedDate + ' ' + additionalData + "</t></is></c></row>";
 
             // Insertar la nueva fila al principio del archivo Excel
-            sheet.childNodes[0].childNodes[1].innerHTML = row + sheet.childNodes[0].childNodes[1].innerHTML;
-
-            // Añadir estilo (negrilla) a las celdas (s="1" referencia al estilo en la hoja de estilos)
-            var styleSheet = xlsx.xl['styles.xml'];
-            var cellXfs = $('cellXfs', styleSheet);
-            cellXfs.append('<xf xfId="0" applyFont="1" fontId="1"/>');
-            var fonts = $('fonts', styleSheet);
-            fonts.append('<font><b/><sz val="11"/><color rgb="000000"/><name val="Calibri"/></font>');
+            sheet.childNodes[0].childNodes[1].innerHTML =
+              row + sheet.childNodes[0].childNodes[1].innerHTML;
           },
         },
         //  Boton para exportar archivos en formato CSV
@@ -148,14 +121,16 @@ document.addEventListener('DOMContentLoaded', async () => {
             },
           },
           customize: function (csv) {
-            var date = new Date();
-            var formattedDate =
-              "Documento Generado: " +
-              date.getFullYear() +
-              "-" +
-              (date.getMonth() + 1) +
-              "-" +
-              date.getDate();
+            var d = new Date();
+            var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+            var time = d.toLocaleString('es-CO', {
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric',
+              hour12: true
+            });
+            var dateTime = date + ' ' + time;
+            var formattedDate = "Documento Generado: " + dateTime;
             var additionalData = "Calificaciones Centro de Actividades";
 
             // Agregar la fecha y el dato adicional como filas en el contenido CSV
@@ -182,14 +157,16 @@ document.addEventListener('DOMContentLoaded', async () => {
             },
           },
           customize: function (doc) {
-            var date = new Date();
-            var formattedDate =
-              "Documento Generado: " +
-              date.getFullYear() +
-              "-" +
-              (date.getMonth() + 1) +
-              "-" +
-              date.getDate();
+            var d = new Date();
+            var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
+            var time = d.toLocaleString('es-CO', {
+              hour: 'numeric',
+              minute: 'numeric',
+              second: 'numeric',
+              hour12: true
+            });
+            var dateTime = date + ' ' + time;
+            var formattedDate = "Documento Generado: " + dateTime;
             var additionalData = "Calificaciones Centro de Actividades";
 
             // Agregar la fecha y el dato adicional como una fila en el contenido del PDF
