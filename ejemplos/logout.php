9a04d27c9cfa96244a790b10dc8d2c6ecc1aaccc
Victor Echeverri <162375245+vme22@users.noreply.github.com>
2024-08-06 11:51:10 -0500

    update estructura

--- ejemplos/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Iniciar sesión
-session_start();
-// Destruir la sesión
-session_destroy();
-echo json_encode(["success" => true]);
-// Redirigir al usuario a la página de inicio de sesión
-header("Location: http://localhost/zajuna/login");
-exit();
